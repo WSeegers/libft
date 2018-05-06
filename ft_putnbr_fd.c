e1cc50efd7b350bc35ff663819866456ed7e2fa5
Werner Seegers <wseegers.mauws@gmail.com>
2018-05-06 10:44:16 +0200

    fixed putnbr bug

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -6,7 +6,7 @@
 /*   By: wseegers <wseegers.mauws@gmail.com>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/26 00:07:21 by wseegers          #+#    #+#             */
-/*   Updated: 2018/05/02 12:30:58 by wseegers         ###   ########.fr       */
+/*   Updated: 2018/05/06 10:42:01 by wseegers         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,16 +16,11 @@ void	ft_putnbr_fd(int nbr, int fd)
 {
 	t_uint unbr;
 	if (nbr < 0)
-	{
 		ft_putchar_fd('-', fd);
-		unbr = -nbr;
-	} else
-		unbr = nbr;
-	if (unbr > 10)
+	unbr = ft_abs(nbr);
+	if (unbr > 0)
 	{
 		ft_putnbr_fd(unbr / 10, fd);
 		ft_putchar_fd((unbr % 10) + '0', fd);
 	}
-	else
-		ft_putchar_fd((unbr % 10) + '0', fd);
 }
